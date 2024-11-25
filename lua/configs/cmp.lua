624088450f71874aaf8c5780e572767ccd2b9658
dankrajnak <dkrajnak@watermarkinsights.com>
2024-11-26 00:34:27 +0700

    actually fix completion behavior

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -1,9 +1,40 @@
 local cmp = require "cmp"
 
 local options = {
-  confirm_opts = {
-    behavior = cmp.ConfirmBehavior.Replace,
-    select = false,
+  -- copied from https://github.com/NvChad/NvChad/blob/bbc3d43db088c141b142a40cd5f717635833a54e/lua/nvchad/configs/cmp.lua
+  -- but changed the confirm behavior
+  mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+
+    ["<CR>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = false,
+    },
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif require("luasnip").expand_or_jumpable() then
+        require("luasnip").expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif require("luasnip").jumpable(-1) then
+        require("luasnip").jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   },
 
   enabled = function()
