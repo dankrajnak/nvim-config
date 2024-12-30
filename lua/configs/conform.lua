116af92db875b67766a7079620ac5c08ae7be342
Dan <dan.krajnak@gmail.com>
2024-12-30 15:35:31 -0500

    support for deno and python

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -2,12 +2,43 @@ local options = {
   formatters_by_ft = {
     lua = { "stylua" },
     rust = { "rustfmt" },
-    css = { "prettier" },
-    html = { "prettier" },
-    typescript = { "prettier" },
-    typescriptreact = { "prettier" },
-    javascript = { "prettier" },
-    markdown = { "prettier" },
+    python = { "ruff_organize_imports", "ruff_fix", "ruff_format" },
+    css = function()
+      if vim.fn.filereadable "deno.json" == 1 or vim.fn.filereadable "deno.jsonc" == 1 then
+        return { "deno_fmt" }
+      end
+      return { "prettier" }
+    end,
+    javascript = function()
+      if vim.fn.filereadable "deno.json" == 1 or vim.fn.filereadable "deno.jsonc" == 1 then
+        return { "deno_fmt" }
+      end
+      return { "prettier" }
+    end,
+    html = function()
+      if vim.fn.filereadable "deno.json" == 1 or vim.fn.filereadable "deno.jsonc" == 1 then
+        return { "deno_fmt" }
+      end
+      return { "prettier" }
+    end,
+    typescript = function()
+      if vim.fn.filereadable "deno.json" == 1 or vim.fn.filereadable "deno.jsonc" == 1 then
+        return { "deno_fmt" }
+      end
+      return { "prettier" }
+    end,
+    typescriptreact = function()
+      if vim.fn.filereadable "deno.json" == 1 or vim.fn.filereadable "deno.jsonc" == 1 then
+        return { "deno_fmt" }
+      end
+      return { "prettier" }
+    end,
+    markdown = function()
+      if vim.fn.filereadable "deno.json" == 1 or vim.fn.filereadable "deno.jsonc" == 1 then
+        return { "deno_fmt" }
+      end
+      return { "prettier" }
+    end,
   },
 
   format_on_save = {
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "rust_analyzer", "eslint", "tailwindcss", "csharp_ls" }
+local servers = { "html", "cssls", "rust_analyzer", "eslint", "tailwindcss", "ruff", "pyright", "csharp_ls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -16,14 +16,14 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+lspconfig.denols.setup {
+  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+}
+
 lspconfig.ts_ls.setup {
+  root_dir = lspconfig.util.root_pattern "package.json",
+  single_file_support = false,
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
-  root_dir = lspconfig.util.root_pattern "package.json",
 }
-
--- lspconfig.denols.setup {
---   on_attach = nvlsp.on_attach,
---   root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
--- }
